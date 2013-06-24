a5c4023d0aec70d907c59a5185d4521fdaec172d
Hendrik Volkmer <h.volkmer@cloudbau.de>
2013-06-24 11:08:42 +0200

    Fixes OpenStack flavor API behaviour
    We want to see all flavors if we call "all". Make
    sure private and public flavors are shown

--- lib/fog/openstack/models/compute/flavors.rb
+++ lib/fog/openstack/models/compute/flavors.rb
@@ -9,8 +9,13 @@ module Fog
 
         model Fog::Compute::OpenStack::Flavor
 
-        def all(options = {})
-          data = service.list_flavors_detail(options).body['flavors']
+        def all
+          public_data = service.list_flavors_detail.body['flavors']
+          # This quirk is needed because the OpenStack API does strange things and does not
+          # show non-public flavors by default (even though the user would have the rights)
+          # to view them
+          non_public_data = service.list_flavors_detail(:is_public => false).body['flavors']
+          data = (public_data + non_public_data).uniq
           load(data)
         end
 
